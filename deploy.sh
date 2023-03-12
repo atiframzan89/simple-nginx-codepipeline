4a8a28d45675849996050aaf888414af07f662ff
Muhammad Atif Ramzan <78168446+atiframzan89@users.noreply.github.com>
2023-03-12 20:13:39 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ set -x
 set -e
 cd /home/ubuntu/
 ls -al
+sudo apt update -y
+sudo apt install nginx -y
 sudo tar -xvf /home/ubuntu/nginx.tar.gz .
 cp index.html /var/www/html/
 sudo systemctl restart nginx
